c97ccff9347aef03de15fff653c20d3923ed4b82
Jonatas Melo Silva <jonatas891024@gmail.com>
2025-06-19 01:58:12 -0300

    feat: atualiza o service worker para suportar caminhos dinâmicos e melhora o registro

--- sw.js
+++ sw.js
@@ -1,23 +1,33 @@
-const CACHE_NAME = 'static-cache-v1'
+const CACHE_NAME = 'static-cache-v2'
+const BASE_PATH = self.location.pathname.startsWith('/portme') ? '/portme' : ''
+
 const ASSETS = [
-  '/src/assets/icones/icone-envelope.svg',
-  '/src/assets/icones/icone-github.svg',
-  '/src/assets/icones/icone-instagram.svg',
-  '/src/assets/icones/icone-linkedin.svg',
-  '/src/assets/icones/icone-quebra-cabeca.svg',
-  '/src/assets/icones/icone-telegram.svg',
-  '/src/assets/icones/icone-whatsapp.svg',
-  '/src/assets/icones/icone-x.svg',
-  '/src/assets/image-avatar-laptop.avif',
-  '/src/assets/image-avatar-mobile.avif',
-  '/src/assets/image-avatar-tablet.avif',
-  '/src/assets/image-logo-sethas-laptop.avif',
-  '/src/assets/image-logo-sethas-mobile.avif',
-  '/src/assets/image-logo-sethas-tablet.avif',
+  `${BASE_PATH}/src/assets/icones/icone-envelope.svg`,
+  `${BASE_PATH}/src/assets/icones/icone-github.svg`,
+  `${BASE_PATH}/src/assets/icones/icone-instagram.svg`,
+  `${BASE_PATH}/src/assets/icones/icone-linkedin.svg`,
+  `${BASE_PATH}/src/assets/icones/icone-quebra-cabeca.svg`,
+  `${BASE_PATH}/src/assets/icones/icone-telegram.svg`,
+  `${BASE_PATH}/src/assets/icones/icone-whatsapp.svg`,
+  `${BASE_PATH}/src/assets/icones/icone-x.svg`,
+  `${BASE_PATH}/src/assets/image-avatar-laptop.avif`,
+  `${BASE_PATH}/src/assets/image-avatar-mobile.avif`,
+  `${BASE_PATH}/src/assets/image-avatar-tablet.avif`,
+  `${BASE_PATH}/src/assets/image-logo-sethas-laptop.avif`,
+  `${BASE_PATH}/src/assets/image-logo-sethas-mobile.avif`,
+  `${BASE_PATH}/src/assets/image-logo-sethas-tablet.avif`,
 ]
 
 self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)))
+  event.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then(cache => {
+        console.log('[SW] Cacheando recursos:', ASSETS)
+        return cache.addAll(ASSETS)
+      })
+      .catch(err => console.error('[SW] Falha ao cachear:', err))
+  )
 })
 
 self.addEventListener('fetch', event => {
