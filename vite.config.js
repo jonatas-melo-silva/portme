0212c7d4222986a675de2b8534379df8bab116c2
Jonatas Melo Silva <jonatas891024@gmail.com>
2025-07-01 10:16:14 -0300

    update: Remove output CSS file and add Vite configuration for Tailwind CSS integration

---
+++ vite.config.js
@@ -0,0 +1,27 @@
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+import tailwindcss from '@tailwindcss/vite'
+
+import { defineConfig } from 'vite'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+export default defineConfig({
+  base: '/portme/',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
+    },
+  },
+  plugins: [tailwindcss()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+    },
+  },
+})
