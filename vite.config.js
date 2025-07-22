ad1cc9d5becd4fb673cab847aae11898675511e4
Jonatas Melo Silva <jonatas891024@gmail.com>
2025-07-22 12:44:39 -0300

    fix: atualiza caminhos de ícones e imagens no cache e ajusta a base do Vite

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ import { defineConfig } from 'vite'
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
-  base: '/portme/',
+  base: '/',
   build: {
     outDir: 'dist',
     emptyOutDir: true,
